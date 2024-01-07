24f3f281e2b47b26b82bb2537ea39a3d35b74588
ViktoriiaYakaba <yakaba@bk.ru>
2024-01-07 12:40:25 +0100

    add

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -10,20 +10,24 @@ const storedForm = JSON.parse(localStorage.getItem(localStorageKey));
 
   form.addEventListener("input", () => {
     const formState = {
-      email: form.elements.email.value,
-      message: form.elements.message.value,
+      email: form.elements.email.value.trim(),
+      message: form.elements.message.value.trim(),
     };
-
     localStorage.setItem(localStorageKey, JSON.stringify(formState));
   });
 
 
   form.addEventListener("submit", (eve) => {
     eve.preventDefault();
+    if (form.elements.email.value === '' || form.elements.message.value === ''){
+    console.log('Please fill in both email and message fields');
+    return;
+  }
     console.log("Data submitted:", JSON.parse(localStorage.getItem(localStorageKey)));
     form.reset();
     localStorage.removeItem(localStorageKey);
   });
+
 
 
 
